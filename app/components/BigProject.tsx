b5fef78bb5a4a739c4f612b3f83299db783893f7
Andy <andre.andre.jazz@gmail.com>
2024-07-14 17:28:59 -0300

    Project

---
+++ app/components/BigProject.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const BigProject = () => {
+    return (
+        <section className="py-[12vh] px-10 md:px-36  ">
+            <div className="container   max-w-[1160px] mx-auto ">
+                <h2 className='text-4xl font-black text-center'>Projetos</h2>
+                <div className='w-[962px] mx-auto mt-10'>
+                    <div className='flex gap-2 items-center w-full h-[40px] border-4 border-black mx-auto'><div className='ms-2 w-4 h-4 rounded-full bg-black'></div><div className='w-4 h-4 rounded-full bg-black'></div><div className='w-4 h-4 rounded-full bg-black'></div></div>
+                    <div className='w-full h-[612px] border-x-4 border-b-4 border-black mx-auto'></div>
+
+                </div>
+                <div className='max-w-[962px] mx-auto mt-24'>
+                    <h3 className='text-2xl font-bold'>Projeto de automação sisu</h3>
+                    <p className='mt-5'>Desenvolvi uma aplicação desktop utilizando o framework Wails.js, com React.js no front-end e Golang no back-end, além do armazenamento de dados com SQLite. <br /><br />
+
+                        Esta aplicação é projetada para gerenciar e distribuir alunos aprovados pelo processo seletivo SISU, bem como criar chamadas para os alunos selecionados. <br /><br />
+
+                        O uso dessas tecnologias permitiu a criação de uma solução eficiente, segura e de alto desempenho, aprimorando significativamente o processo de gestão e seleção de alunos, substituindo totalmente a utilização do excel pela faculdade.</p>
+                </div>
+
+            </div>
+        </section>
+    )
+}
+
+export default BigProject
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
+import BigProject from "./components/BigProject";
 
 export default function Home() {
   return (
     <main>
       <NavBar />
       <Hero/>
+      <BigProject/>
       <section>
         <h1>teste</h1>
       </section>
